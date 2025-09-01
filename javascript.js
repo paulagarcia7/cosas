9830a8f4d084908d87a9b39b43e412881b282c27
paulagarcia7 <anapaulapizarro@icloud.com>
2025-09-01 10:45:02 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// let variable1 = 5
+// let variable2 = 5 
+// console.log(variable1>variable2)
+
+// const fechaNacimiento = console.log("ingrese el año en el que nacio: ")
+// const edad = 0
+function EdadBoliche (fechaNacimiento) {
+    if (fechaNacimiento <= 2006) {
+        console.log("Felicitaciones es mayor de edad")
+    } else {
+        console.log("no es mayor de edad")
+    }
+}
+console.log(EdadBoliche(2008))
+
